73e72f4edaa3dff2aa6991b808e40667304afed7
rogercg <tony2006cg@gmail.com>
2020-03-16 22:48:20 -0500

    Modulo organizador de AngularMaterial y uso de botones

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+// Modulo generado con la linea de comandos. Este modulo servirá para organizar los modulos  de Angular Material
+
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';// Componente a usar
+
+const MaterialComponents = [
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
